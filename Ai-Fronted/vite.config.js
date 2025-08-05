c454cbe2a33ee7162ca080e81d3f162541de829a
Gulshan kumar <ronyahuja806@gmail.com>
2025-08-05 15:27:13 +0530

    update

--- Ai-Fronted/vite.config.js
+++ Ai-Fronted/vite.config.js
@@ -4,5 +4,6 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss(),],base: "/Talk-To-Ai/" 
+  plugins: [react(), tailwindcss(),],
+  base: "/Talk-To-Ai/" 
 })
