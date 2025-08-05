ffd9b4fca556f0a5bb4505f092d7304800f1b5bd
Gulshan kumar <ronyahuja806@gmail.com>
2025-08-05 15:38:08 +0530

    Setup for GitHub Pages

--- Ai-Fronted/vite.config.js
+++ Ai-Fronted/vite.config.js
@@ -5,5 +5,5 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss(),],
-  base: '/Talk-To-Ai/', // 👈 Your GitHub repo name with trailing slashes
+  base: '/Talk-To-Ai/' // 👈 Your GitHub repo name with trailing slashes
 });
